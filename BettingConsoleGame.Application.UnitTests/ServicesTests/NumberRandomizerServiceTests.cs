1770dd01371dc1d7fecdf7216747fcddff6a611a
Simeon Vanov <simooovanov@gmail.com>
2022-04-24 21:26:04 +0300

    add number randomizer service tests

---
+++ BettingConsoleGame.Application.UnitTests/ServicesTests/NumberRandomizerServiceTests.cs
@@ -0,0 +1,35 @@
+﻿using BettingConsoleGame.Application.Services.Randomize;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace BettingConsoleGame.Application.UnitTests.Services
+{
+    public class NumberRandomizerServiceTests
+    {
+        private NumberRandomizerService sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            this.sut = new NumberRandomizerService();
+        }
+
+        [Test]
+        public void Should_NotDuplicateRandomDouble_When_CalledTwice()
+        {
+            var first = this.sut.GetRandomDouble();
+            var second = this.sut.GetRandomDouble();
+
+            first.Should().NotBe(second);
+        }
+
+        [Test]
+        public void Should_ReturnRandomDoubleInRange_When_MinMaxIsSpecified()
+        {
+            var first = this.sut.GetRandomDouble(50, 55);
+
+            first.Should().BeGreaterThanOrEqualTo(50);
+            first.Should().BeLessThan(55);
+        }
+    }
+}
